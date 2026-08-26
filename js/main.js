837ea1cd8021d5af1658ea61174fc44868d7b427
Mini-17 <2551050174phuc@ou.edu.vn>
2026-08-27 00:28:47 +0700

    them tinh nang lo dan khi cuon

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 import { initTheme } from "./theme.js";
 import { initBackToTop } from "./backToTop.js";
 import { initHeader } from "./header.js";
-
+import { initScrollReveal } from "./scrollReveal.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   initTheme();
   initBackToTop();
   initHeader();
+  initScrollReveal();
 });
