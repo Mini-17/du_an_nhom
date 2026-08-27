ea8fc2781dcdfa60b6b51980038a893dfccebd43
Mini-17 <2551050174phuc@ou.edu.vn>
2026-08-27 14:37:10 +0700

    them du lieu sach, thu gon tinh nag chung vao mot folder

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-import { initTheme } from "./theme.js";
-import { initBackToTop } from "./backToTop.js";
-import { initHeader } from "./header.js";
-import { initScrollReveal } from "./scrollReveal.js";
+import { initTheme } from "./modules/theme.js";
+import { initBackToTop } from "./modules/backToTop.js";
+import { initHeader } from "./modules/header.js";
+import { initScrollReveal } from "./modules/scrollReveal.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   initTheme();
